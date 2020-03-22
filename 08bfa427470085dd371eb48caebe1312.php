d66ab21cd102011bc91cfb4f09f26ca39a576331
叶华 <799475742@qq.com>
2020-03-22 10:44:33 +0800

    update readme

---
+++ 08bfa427470085dd371eb48caebe1312.php
@@ -0,0 +1,11 @@
+<?php /*a:1:{s:74:"/Users/yehua/Desktop/easyswoole/think/easyswoole-template-think/think.html";i:1584793086;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test-think</title>
+</head>
+<body>
+    i'm render by <?php echo htmlentities($engine); ?>
+</body>
+</html>
